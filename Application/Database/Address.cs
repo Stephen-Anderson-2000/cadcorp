a29f9eb75d3acf9b623537821577eedd19c08918
Stephen Anderson <steppyandrews@gmail.com>
2024-04-22 09:17:49 +0100

    Restructures the project and changes the database implementation to use EFCore (WIP)

---
+++ Application/Database/Address.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cadcorp.Database;
+
+[Table("Address")]
+public class Address
+{
+    [Column("id")]
+    [Key]
+    public int Id { get; set; }
+    [Column("line1")]
+    public required string Line1 { get; set; }
+    [Column("line2")]
+    public string? Line2 { get; set; }
+    [Column("line3")]
+    public string? Line3 { get; set; }
+    [Column("town")]
+    public required string Town { get; set; }
+    [Column("postcode")]
+    public required string PostCode { get; set; }
+}
---
+++ Application/Database/AddressContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cadcorp.Database;
+public class AddressContext(DbContextOptions<AddressContext> options) : DbContext(options)
+{
+    public DbSet<Address> Addresses { get; set; }
+}
