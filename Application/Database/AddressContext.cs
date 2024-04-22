87fe8066f0ba666f52166b92513b93ade338e604
Stephen Anderson <steppyandrews@gmail.com>
2024-04-22 13:29:53 +0100

    Removes database changes from the branch to separate work

--- Application/Database/AddressContext.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Cadcorp.Database;
-public class AddressContext(DbContextOptions<AddressContext> options) : DbContext(options)
-{
-    public DbSet<Address> Addresses { get; set; }
-}
--- Application/Program.cs
+++ Application/Program.cs
@@ -2,6 +2,3 @@
 
 const string FILE_PATH = "data.csv";
 var addresses = FileUtility.ReadAddressCSV(FILE_PATH);
-var insertCount = DatabaseUtility.InsertAddresses(addresses);
-
-Console.WriteLine($"{insertCount} row(s) inserted");
